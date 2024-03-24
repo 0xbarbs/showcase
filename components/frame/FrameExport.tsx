1be728d63952457bdb98860dd857f4fbb86e4b57
0xbarbs <0xbarbs@proton.me>
2024-03-24 03:04:20 +0000

    feat: added post-export instructions

--- components/frame/FrameExport.tsx
+++ components/frame/FrameExport.tsx
@@ -1,6 +1,8 @@
-import { FaDownload } from "react-icons/fa";
-import { Button } from "@/components/ui/button";
 import axios from "axios";
+import { useState } from "react";
+import { FaDownload } from "react-icons/fa";
+
+import { Button } from "@/components/ui/button";
 import { FrameData } from "@/types/frame";
 
 export function FrameExport({
@@ -8,6 +10,8 @@ export function FrameExport({
 }: {
   frameData: Record<number, FrameData>
 }) {
+  const [exported, setExported] = useState(false);
+
   async function getDownload() {
     const blob = await axios.post('api/export', frameData, {
       responseType: "blob",
@@ -23,12 +27,21 @@ export function FrameExport({
     document.body.appendChild(a);
     a.click();
     window.URL.revokeObjectURL(url);
+    setExported(true);
   }
 
   return (
-    <Button onClick={getDownload} variant="outline" className="mt-6">
-      <FaDownload className="mr-2" />
-      Export frame files
-    </Button>
+    <div className="flex flex-col">
+      <Button onClick={getDownload} variant="outline" className="mt-6">
+        <FaDownload className="mr-2" />
+        Export frame files
+      </Button>
+
+      {exported && (
+        <p className="mt-4 max-w-[500px] text-center self-center text-sm">
+          Frames exported! Extract the contents into a <span className="font-bold">.gitshowcase</span> directory located at the root of your repository. Don't forget to commit and push.
+        </p>
+      )}
+    </div>
   )
 }
