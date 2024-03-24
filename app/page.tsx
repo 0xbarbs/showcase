497e8432ea25e774f0fe6db168aa04cd17107f3f
0xbarbs <0xbarbs@proton.me>
2024-03-24 03:23:26 +0000

    feat: added link to frame builder on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { IoBuild } from "react-icons/io5";
 
 export default function Home() {
   return (
@@ -14,9 +16,13 @@ export default function Home() {
         <div className="absolute left-0 bg-green-800 text-white py-2 px-4 rounded-full text-xl font-bold">1</div>
 
         <h2 className="mt-1 text-2xl w-[600px] font-bold">Create your frame</h2>
-        <p className="text-lg mt-2">Use our simple interactive frame builder to create your show case.</p>
+        <p className="text-lg mt-2">Use our simple interactive frame builder to create your showcase.</p>
+
+        <Link href="/create">
+          <Button className="mt-4"><IoBuild size={18} className="mr-2" /> Open Frame Builder</Button>
+        </Link>
       </section>
-      <section className="mt-16 pl-14 relative">
+      <section className="mt-12 pl-14 relative">
         <div className="absolute left-0 bg-green-800 text-white py-2 px-4 rounded-full text-xl font-bold">2</div>
 
         <h2 className="mt-1 text-2xl w-[600px] font-bold">Commit and push</h2>
