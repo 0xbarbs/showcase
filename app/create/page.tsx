bf21485cea7b5ab8cf00e167e441e3c86c14d10b
0xbarbs <0xbarbs@proton.me>
2024-03-24 10:54:51 +0000

    refactor: remove redundant old code

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,26 +1,11 @@
 "use client";
 
-import { useState } from "react";
-
 import { Frame } from "@/components/frame";
-import { FrameData } from "@/types/frame";
 
 export default function Home() {
-  const [frameData, setFrameData] = useState<Record<number, FrameData>>({});
-  const [activeFrame, setActiveFrame] = useState(0);
-
-  const onSubmit = (data: any) => {
-    console.log({ data: data.image });
-
-    setFrameData(f => ({ ...f, [activeFrame]: data }));
-  }
-
   return (
     <main className="flex w-screen h-screen justify-center items-center">
-      <Frame
-        onSubmit={onSubmit}
-        data={frameData[activeFrame]}
-      />
+      <Frame />
     </main>
   )
 }
